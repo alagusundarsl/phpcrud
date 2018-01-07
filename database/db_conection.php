a39db1fa9f75554a3a4268ff8f6610123af08cc1
alagusundarsl <34939005+alagusundarsl@users.noreply.github.com>
2018-01-07 20:04:35 +0530

    Update db_conection.php

--- database/db_conection.php
+++ database/db_conection.php
@@ -1,6 +1,6 @@
 <?php
 $con=mysqli_init(); 
 mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-mysqli_real_connect($con, "sundarphpwork.mysql.database.azure.com", "alagu123@sundarphpwork", {suntest123$}, {users}, 3306);
+mysqli_real_connect($con, "sundarphpwork.mysql.database.azure.com", "alagu123@sundarphpwork", {suntest123$}, {sundarphpwork}, 3306);
 
 ?>
